accb320a0837247ee370991680d2e728cbe95923
Ahmed Kamel <ahmedkamel180998@gmail.com>
2025-04-27 14:35:21 +0300

    initial commit

---
+++ external.js
@@ -0,0 +1,11 @@
+let number1 = 3,
+  number2 = 2.9,
+  number3 = 0xff;
+
+var firstName = "Ahmed",
+  middleName = "Kamel",
+  lastName = `Elwazeer`;
+
+let isTrue = true;
+
+console.log(`This is the External JS file`);
